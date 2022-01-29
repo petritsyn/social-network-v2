551d23525ed73aae45596270f823181d4ff0d160
youngvvp <andrey2343@yandex.ru>
2022-01-30 02:39:27 +0300

    15 - new components, posts

---
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Post from "./Post/Post";
+
+const MyPosts = () => {
+    return (
+        <div>
+            <Post/>
+            <Post/>
+        </div>
+    )
+}
+
+export default MyPosts;
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import s from './Navbar.module.css'
+import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
         <div className={s.profile}>
-            Profile
+            <MyPosts/>
         </div>
     )
 }
